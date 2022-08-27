270b721008c3b1c07d9cf2d556a83dafa690ef66
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2022-08-27 22:33:52 +0300

    add third task

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,50 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const ANIMATION_DURATION = 200;
+
+const ANIMATION_OPTIONS = {
+  fill: 'forwards',
+  duration: ANIMATION_DURATION
+};
+
+const collapsibleButton = document.querySelector('.collapsible__button'),
+      collapsibleContent = document.querySelector('.collapsible__content'),
+      buttonTitleVisible = document.querySelector('.collapsible__action--visible'),
+      buttonTitleHidden = document.querySelector('.collapsible__action--hidden');
+
+const hideContentKeyframes = [
+        { opacity: 1 },
+        { opacity: 0 }
+      ],
+      showContentKeyframes = [
+        { opacity: 0 },
+        { opacity: 1 }
+      ];      
+
+
+buttonTitleHidden.style.display = 'none';
+let isContentVisible = true;
+
+function hideContent() {
+  collapsibleContent.style.display = 'none';
+}
+
+collapsibleButton.addEventListener('click', () => {
+  if(isContentVisible) {
+    buttonTitleVisible.style.display = 'none';
+    buttonTitleHidden.style.display = 'block';
+
+    const hideContentAnimation = collapsibleContent.animate(hideContentKeyframes, ANIMATION_OPTIONS);
+    hideContentAnimation.onfinish = hideContent;
+
+    isContentVisible = false;
+
+    return;
+  }
+
+  buttonTitleVisible.style.display = 'block';
+  buttonTitleHidden.style.display = 'none';
+
+  collapsibleContent.style.display = 'block';
+  collapsibleContent.animate(showContentKeyframes, ANIMATION_OPTIONS);
+  
+  isContentVisible = true;
+})
